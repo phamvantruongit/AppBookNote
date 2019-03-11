6de47040112174dbe71680a951049f4004e13ad4
mac <truongpham@bravesoft.com.vn>
2019-03-11 17:12:08 +0700

    cm app

--- app/src/main/java/vn/com/it/truongpham/appbooknote/MainActivity.java
+++ app/src/main/java/vn/com/it/truongpham/appbooknote/MainActivity.java
@@ -195,22 +195,6 @@ public class MainActivity extends BaseActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-//        int id = item.getItemId();
-//
-//        if (id == R.id.nav_camera) {
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
